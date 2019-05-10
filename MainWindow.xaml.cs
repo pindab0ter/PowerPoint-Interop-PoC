15027ff0dd6826e6e6e0a4fe73d4029509842253
pindab0ter <hansvanluttikhuizen@me.com>
2019-05-10 14:12:03 +0200

    Initial commit
    Remove Amfors references

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows;
+using Microsoft.Office.Core;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using System;
+using System.Windows.Controls;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+namespace PowerPoint_Interop_PoC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly PowerPoint.Application application = new PowerPoint.Application();
+        readonly PowerPoint.Presentation presentation;
+        readonly PowerPoint.SlideShowSettings slideShowSettings;
+        readonly PowerPoint.SlideShowWindow slideShowWindow;
+        readonly PowerPoint.Slides slides;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+            presentation = application.Presentations.Open(path + @"\presentation.pptx", ReadOnly: MsoTriState.msoTrue, WithWindow: MsoTriState.msoFalse);
+            slideShowSettings = presentation.SlideShowSettings;
+            slideShowSettings.Run();
+
+            slideShowWindow = application.SlideShowWindows[1];
+            slides = presentation.Slides;
+
+            // Fill combobox with slides
+            Dispatcher.Invoke(() =>
+            {
+                foreach (PowerPoint.Slide slide in slides)
+                {
+                    cbSlides.Items.Add(slide.Name);
+                }
+
+                cbSlides.SelectedIndex = 0;
+            });
+
+            application.SlideShowNextSlide += Application_SlideShowNextSlide;
+            cbSlides.SelectionChanged += ComboBox_SelectionChanged;
+            btnBackgroundcolour.Click += BtnBackgroundcolour_Click;
+        }
+
+        private void BtnBackgroundcolour_Click(object sender, RoutedEventArgs e)
+        {
+            PowerPoint.Slide currentSlide = slides[slideShowWindow.View.CurrentShowPosition];
+            currentSlide.FollowMasterBackground = MsoTriState.msoFalse;
+            currentSlide.Background.Fill.ForeColor.RGB = ColorTranslator.ToOle(Color.Red);
+        }
+
+        private void Application_SlideShowNextSlide(PowerPoint.SlideShowWindow Wn)
+        {
+            int currentShowPosition = Wn.View.CurrentShowPosition;
+            Console.WriteLine($"User triggered next to move to the next slide ({currentShowPosition})");
+
+            this.Dispatcher.Invoke(() =>
+            {
+                cbSlides.SelectedIndex = currentShowPosition - 1;
+            });
+        }
+
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            slideShowWindow.View.GotoSlide(comboBox.SelectedIndex + 1);
+        }
+    }
+}
