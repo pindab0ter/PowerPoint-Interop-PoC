118899a39e969a712de5fa7b9987114c697a1c3b
pindab0ter <hansvanluttikhuizen@me.com>
2019-06-27 11:11:44 +0200

    Reorganise code and add comments

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace PowerPoint_Interop_PoC
     /// </summary>
     public partial class MainWindow : Window
     {
-        readonly PowerPoint.Application application = new PowerPoint.Application();
+        readonly PowerPoint.Application powerPoint = new PowerPoint.Application();
         readonly PowerPoint.Presentation presentation;
         readonly PowerPoint.SlideShowSettings slideShowSettings;
         readonly PowerPoint.SlideShowWindow slideShowWindow;
@@ -25,11 +25,11 @@ namespace PowerPoint_Interop_PoC
             InitializeComponent();
             string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
 
-            presentation = application.Presentations.Open(path + @"\presentation.pptx", ReadOnly: MsoTriState.msoTrue, WithWindow: MsoTriState.msoFalse);
+            presentation = powerPoint.Presentations.Open(path + @"\presentation.pptx", ReadOnly: MsoTriState.msoTrue, WithWindow: MsoTriState.msoFalse);
             slideShowSettings = presentation.SlideShowSettings;
             slideShowSettings.Run();
 
-            slideShowWindow = application.SlideShowWindows[1];
+            slideShowWindow = powerPoint.SlideShowWindows[1];
             slides = presentation.Slides;
 
             // Fill combobox with slides
@@ -43,19 +43,29 @@ namespace PowerPoint_Interop_PoC
                 cbSlides.SelectedIndex = 0;
             });
 
-            application.SlideShowNextSlide += Application_SlideShowNextSlide;
+            // Subscribe to events
             cbSlides.SelectionChanged += ComboBox_SelectionChanged;
-            btnBackgroundcolour.Click += BtnBackgroundcolour_Click;
+            btnBackgroundColor.Click += BtnBackgroundColor_Click;
+            powerPoint.SlideShowNextSlide += OnSlideShowNextSlide;
         }
 
-        private void BtnBackgroundcolour_Click(object sender, RoutedEventArgs e)
+        //
+        // Application Window Events
+        //
+
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            slideShowWindow.View.GotoSlide(comboBox.SelectedIndex + 1);
+        }
+
+        private void BtnBackgroundColor_Click(object sender, RoutedEventArgs e)
         {
             PowerPoint.Slide currentSlide = slides[slideShowWindow.View.CurrentShowPosition];
             currentSlide.FollowMasterBackground = MsoTriState.msoFalse;
-
             PowerPoint.ColorFormat backgroundColor = currentSlide.Background.Fill.ForeColor;
 
-            Console.WriteLine(backgroundColor.RGB);
+            // Toggle between colors
             if (backgroundColor.RGB == ColorTranslator.ToOle(Color.White))
             {
                 backgroundColor.RGB = ColorTranslator.ToOle(Color.LightSteelBlue);
@@ -66,7 +76,11 @@ namespace PowerPoint_Interop_PoC
             }
         }
 
-        private void Application_SlideShowNextSlide(PowerPoint.SlideShowWindow Wn)
+        //
+        // PowerPoint Events
+        //
+
+        private void OnSlideShowNextSlide(PowerPoint.SlideShowWindow Wn)
         {
             int currentShowPosition = Wn.View.CurrentShowPosition;
             Console.WriteLine($"User triggered next to move to the next slide ({currentShowPosition})");
@@ -75,12 +89,6 @@ namespace PowerPoint_Interop_PoC
             {
                 cbSlides.SelectedIndex = currentShowPosition - 1;
             });
-        }
-
-        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            ComboBox comboBox = sender as ComboBox;
-            slideShowWindow.View.GotoSlide(comboBox.SelectedIndex + 1);
         }
     }
 }
